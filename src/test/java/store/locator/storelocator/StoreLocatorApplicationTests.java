e6f0e3130435d54495ecdc01113dd5fc57241876
pulkit <pulkit91b@gmail.com>
2017-07-15 01:24:17 +0530

    first commit

---
+++ src/test/java/store/locator/storelocator/StoreLocatorApplicationTests.java
@@ -0,0 +1,16 @@
+package store.locator.storelocator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreLocatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
